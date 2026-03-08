552934cd1e5ef2c85d97e79f803db393027508ee
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2026-03-08 16:05:39 -0400

    Enable ConsumetScraper by setting disabled to false

--- src/providers/sources/consumet/index.ts
+++ src/providers/sources/consumet/index.ts
@@ -67,7 +67,7 @@ export const ConsumetScraper = makeSourcerer({
   id: 'consumet',
   name: 'Consumet (Anime) 🔥',
   rank: 5,
-  disabled: true,
+  disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeShow: consumetScraper,
 });
