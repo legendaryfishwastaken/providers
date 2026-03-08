62da2af88d698161be31c420d765e253f8ce4b26
legendary.fish <129801050+legendaryfishwastaken@users.noreply.github.com>
2026-03-08 16:37:19 -0400

    Change rank of ConsumetScraper from 5 to 42

--- src/providers/sources/consumet/index.ts
+++ src/providers/sources/consumet/index.ts
@@ -66,7 +66,7 @@ async function consumetScraper(ctx: ShowScrapeContext): Promise<SourcererOutput>
 export const ConsumetScraper = makeSourcerer({
   id: 'consumet',
   name: 'Consumet (Anime) 🔥',
-  rank: 5,
+  rank: 42,
   disabled: false,
   flags: [flags.CORS_ALLOWED],
   scrapeShow: consumetScraper,
